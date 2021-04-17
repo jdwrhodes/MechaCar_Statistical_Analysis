d650ecb5394b2f7c1b8f7996a1d4a6d0c8ba5819
Joshua Rhodes <jdw.rhodes@gmail.com>
2021-04-17 11:24:44 -0500

    added additional screenshot

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,9 @@ data <- read.csv('./data/MechaCar_mpg.csv', check.names = F, stringsAsFactors = 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = data)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = data))
+
+coil_data <- read.csv('./data/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+total_summary <- coil_data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+lot_summary <- coil_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
